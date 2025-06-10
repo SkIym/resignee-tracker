ef0a2ed011ad4a588800e5f0c806ec169aa3743d
Aneko Delfin <anekodelfin@Anekos-MacBook-Air.local>
2025-06-10 16:03:41 +0800

    Initial frontend commit

---
+++ frontend/src/lib/Table.svelte
@@ -0,0 +1,20 @@
+<table>
+    <thead class="new-thead z-10">
+        <tr>
+            <th class="course">Course</th>
+            <th class="type">Type</th>
+            <th class="section">Section</th>
+            <th class="days">Day</th>
+            <th class="time">Start Time</th>
+            <th class="time">End Time</th>
+            <th class="room">Room</th>
+            <th class="size">Size</th>
+            <th class="instructor">Instructor</th>
+            <th class="load">Load</th>
+			<th class="status">Status</th>
+			<th>+/- Slots</th>
+            <th>Delete</th>
+			<th>Edit</th>
+        </tr>
+    </thead>
+</table>
