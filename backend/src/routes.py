918c8962b4ea1760df9e352061209e18bc446443
Skiym <abramjosh13@gmail.com>
2025-06-10 17:12:33 +0800

    chore: add comments for future functions

--- backend/src/routes.py
+++ backend/src/routes.py
@@ -1,1 +1,9 @@
 # list endpoints here
+
+# Endpoint accepting raw text (details) of resignees; will parse and add data to database
+
+# Endpoint serving list of unprocessed resignees to client (frontend) 
+
+# Endpoint to mark resignation entry as processed (will now not be returned to client )
+
+# Endpoint serving report of processed resignees within a selected timeframe
--- backend/src/services.py
+++ backend/src/services.py
@@ -1,1 +1,7 @@
 # place parsing functions and other business logic here
+
+
+# Parsing function (from raw text from email to labeled data)
+
+
+# Encoding data to XCEL file function (for reports)
