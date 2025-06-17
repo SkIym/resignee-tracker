208fa55233b7f781a98a39e39486a32fb0131076
SkIym <abramjosh13@gmail.com>
2025-06-17 14:04:35 +0800

    adjust path names

--- backend/src/app.py
+++ backend/src/app.py
@@ -3,8 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.requests import Request
 from fastapi.responses import Response
 from fastapi.responses import JSONResponse
-from services import verify_token
-from routes import router
+from src.services import verify_token
+from src.routes import router
 from typing import Callable, Awaitable
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
@@ -38,11 +38,11 @@ async def auth_middleware(request: Request, call_next: Callable[[Request], Await
             return JSONResponse(status_code=401, content={"detail": "Unauthorized"})
     return await call_next(request)
 
-app.mount("/", StaticFiles(directory="../static", html=True), name="static")
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 @app.get("/{full_path:path}")
 async def serve_spa(full_path: str):
-    static_path = Path("../static/index.html")
+    static_path = Path("static/index.html")
     if not static_path.exists():
         return {"message": "Frontend not built"}
     return FileResponse(static_path)
--- backend/src/services.py
+++ backend/src/services.py
@@ -1,7 +1,7 @@
 # place parsing functions and other business logic here
 
 # Parsing function (from raw text from email to labeled data)
-from schemas import ResigneeCreate
+from src.schemas import ResigneeCreate
 from io import BytesIO
 import xlsxwriter
 from datetime import datetime
